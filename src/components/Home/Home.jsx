dc028fe8cd8dc394bc23af62c1be57b7178bb9c4
jrodr117 <jrodr113@msudenver.edu>
2022-05-22 15:10:15 -0600

    home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,16 @@
 import './Home.css';
-
+import cloud from '../../images/cloud.png';
+import mountain from '../../images/mountain.png';
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div class="top-container">
+			<img class="top-cloud" src={cloud} alt="cloud-img" />
+			<h1>I'm Julian</h1>
+			<h2 class="intro-h2">a Software Engineer</h2>
+			<img class="bottom-cloud" src={cloud} alt="cloud-img" />
+			<img class="mountain" src={mountain} alt="mountain -img" />
+		</div>
+	);
 };
 
 export default Home;
